84905842b4ba1a1840ded0a48235cdb444889140
salman-ctrl <zirmanvictory@gmail.com>
2025-03-01 23:47:37 +0700

    edit hero

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -30,27 +30,34 @@ const Hero = () => {
 
 
     return (
-            <div className="flex h-full z-20">
-                <div className="w-1/2 ml-10 flex flex-col justify-center px-12">
-                    <h1 className="text-5xl text-white font-bold mb-2">
-                        {headline}
-                    </h1>
-                    <p className="text-4xl text-gray-300 mt-4">
-                        {paragraph}
-                    </p>
-                    <button className="px-8 mt-16 py-4 text-black text-2xl bg-white rounded-xl hover:text-white hover:bg-black  hover:bg-black transition duration-500 w-max z-20 flex items-center gap-2 transition duration-300 hover:scale-110">
-                        <a href="#about">Learn More!</a> <ArrowRight />
-                    </button>
-                </div>
-
-                <div className="relative w-1/2 flex justify-center items-end">
-                    <img
-                        src="/assets/foto ku.png"
-                        alt="fotosaya"
-                        className="fotoku absolute bottom-0 max-h-full h-[850px]  w-[700px] object-cover  rounded-xl hover:scale-105  transition duration-1000"
-                    />
-                </div>
+        <div className="flex h-full z-20">
+            <div className="w-1/2 ml-10 flex flex-col justify-center px-12">
+                <h1 className="text-5xl text-white font-bold mb-2">
+                    {headline}
+                </h1>
+                <p className="text-4xl text-gray-300 mt-4">
+                    {paragraph}
+                </p>
+                <button className="px-8 mt-16 py-4 text-black text-2xl bg-white rounded-xl hover:text-white hover:bg-black  hover:bg-black transition duration-500 w-max z-20 flex items-center gap-2 transition duration-300 hover:scale-110">
+                    <a href="#about">Learn More!</a> <ArrowRight />
+                </button>
             </div>
+
+            <div className="relative w-1/2 flex justify-center items-end">
+                <img
+                    src="/assets/foto ku.png"
+                    alt="fotosaya"
+                    className="fotoku absolute bottom-0 max-h-full h-[850px]  w-[700px] object-cover  rounded-xl hover:scale-105  transition duration-1000"
+                />
+            </div>
+            <div className="relative w-1/2 flex justify-center items-end">
+                <img
+                    src="/assets/foto ku.png"
+                    alt="fotosaya"
+                    className="fotoku absolute bottom-0 max-h-full h-[850px]  w-[700px] object-cover  rounded-xl hover:scale-105  transition duration-1000"
+                />
+            </div>
+        </div>
     )
 }
 
