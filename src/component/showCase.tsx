f9a9f9d591706df4e9fedebb4329e6dd26e9016c
salman-ctrl <zirmanvictory@gmail.com>
2025-11-13 23:19:17 +0700

    edit

--- src/component/showCase.tsx
+++ src/component/showCase.tsx
@@ -46,7 +46,7 @@ const mockData = [
                 "date": "2024-04-05",
                 "company": "Griya Digital",
                 "description": "A landing portfolio for a digital company that showcases their services and featured projects",
-                "technologies": ["HTML", "Bootstrap", "JavaScript"],
+                "technologies": ["HTML", "", "JavaScript"],
                 "link": "https://salman-ctrl.github.io/Company_Profile_Griya_Senandika/"
             },
             {
